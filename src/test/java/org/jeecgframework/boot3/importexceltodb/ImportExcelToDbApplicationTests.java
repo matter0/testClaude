d9ac1e5fc355d675a22f1404c9e2f6e50dd7b1ef
chensunan.170589 <chensunan@halosee.com>
2026-03-19 15:43:25 +0800

    Initial commit

---
+++ src/test/java/org/jeecgframework/boot3/importexceltodb/ImportExcelToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeecgframework.boot3.importexceltodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportExcelToDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
